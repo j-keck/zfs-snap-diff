f0fafe84a3295975c9d19c13e8e45dc1824e379f
j-keck <jhyphenkeck@gmail.com>
2014-09-10 09:52:17 +0200

    add build script

---
+++ build.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+#
+# quick and dirty build script
+#
+use v5.14;
+use strict;
+use warnings;
+use diagnostics;
+use IO::Compress::Zip qw(zip $ZipError);
+
+# get version from git
+my $version = `git describe`;
+chomp($version);
+
+# bindata.go
+say "generate bindata.go ...";
+say `go-bindata -ignore=.git webapp/...`;
+
+
+# recreate build-output
+unlink glob "build-output/*";
+rmdir("build-output");
+mkdir("build-output");
+
+# build
+for my $os(<freebsd linux>){
+    for my $arch(<i386 amd64>){
+        # set build env
+        $ENV{"GOOS"} = $os;
+        $ENV{"GOARCH"} = ($arch eq "i386" ? "386" : $arch);
+
+        # build it
+        my $cmd = qq{go build -ldflags "-X main.VERSION $version" -o zfs-snap-diff};
+        say "build for $os/$arch";
+        say $cmd;
+        system($cmd) && die "build error";
+
+        # pack it
+        zip "zfs-snap-diff" => "build-output/zfs-snap-diff-${os}-${arch}.zip" || die "zip falied: $ZipError\n";
+
+        # delete org binary
+        unlink "zfs-snap-diff" || die $!;
+    }
+}
