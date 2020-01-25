bae0cf775eb46634223bb9f35db6f25f42084618
j-keck <jhyphenkeck@gmail.com>
2020-01-25 08:03:45 +0100

    add more utility functions

--- webapp/src/ZSD/Ops.purs
+++ webapp/src/ZSD/Ops.purs
@@ -2,8 +2,13 @@
 module ZSD.Ops where
 
 import Data.Array as A
+import Data.Foldable (class Foldable)
+import Data.Foldable as F
+import Data.Maybe (Maybe(..), fromJust, maybe)
+import Data.Semigroup (class Semigroup)
 import Data.Tuple (Tuple(..))
-import Prelude (class Applicative, class Bind, class Functor, map, (-), (<$>), (<*>), (>>>))
+import Partial.Unsafe (unsafePartial)
+import Prelude (class Applicative, class Bind, class Functor, map, ($), (-), (<$>), (<*>), (<>), (>>>))
 
 
 mapmap :: forall f1 f2 a b. Functor f1 => Functor f2 => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
@@ -22,3 +27,15 @@ tupleM a b = Tuple <$> a <*> b
 
 zipWithIndex :: forall a. Array a -> Array (Tuple Int a)
 zipWithIndex as = A.zipWith Tuple (A.range 0 (A.length as - 1)) as
+
+
+foldlSemigroup :: forall a t. Foldable t => Semigroup a => t a -> Maybe a
+foldlSemigroup = F.foldl (\b a -> maybe (Just a) (\b' -> Just $ b' <> a) b) Nothing
+
+
+foldrSemigroup :: forall a t. Foldable t => Semigroup a => t a -> Maybe a
+foldrSemigroup = F.foldr (\a b -> maybe (Just a) (\b' -> Just $ a <> b') b) Nothing
+
+
+unsafeFromJust :: forall a. Maybe a -> a
+unsafeFromJust a = unsafePartial $ fromJust a
