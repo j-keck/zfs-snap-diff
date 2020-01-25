0bd3b11a04a32c250a48cb9d4aeaf9086ae75854
j-keck <jhyphenkeck@gmail.com>
2020-01-25 10:24:01 +0100

    highlight selected snaphot in 'SnapshotSelector'

--- webapp/src/ZSD/Views/BrowseSnapshots/SnapshotSelector.purs
+++ webapp/src/ZSD/Views/BrowseSnapshots/SnapshotSelector.purs
@@ -1,10 +1,12 @@
 module ZSD.Views.BrowseSnapshots.SnapshotSelector where
 
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Prelude (Unit, ($), discard)
 import React.Basic (Component, JSX, createComponent, empty, make)
 import React.Basic.DOM as R
-import ZSD.Component.Table (table)
+import ZSD.Component.TableX (tableX)
 import ZSD.Components.Panel (panel)
 import ZSD.Formatter as Formatter
 import ZSD.Model.Snapshot (Snapshots, Snapshot)
@@ -14,7 +16,8 @@ type Props =
   , onSnapshotSelected :: Snapshot -> Effect Unit
   }
 
-type State = {}
+type State =
+  { selectedIdx :: Maybe Int}
 
 
 snapshotSelector :: Props -> JSX
@@ -24,19 +27,21 @@ snapshotSelector = make component { initialState, render }
     component :: Component Props
     component = createComponent "SelectSnapshot"
 
-    initialState = {}
+    initialState = { selectedIdx: Nothing }
 
     render self =
       panel
       { header: R.text "Snapshots"
       , body: \hidePanelBodyFn ->
-        table
+        tableX
          { header: ["Snapshot Name", "Snapshot Created"]
          , rows: self.props.snapshots
          , mkRow: \s -> [ R.text s.name, R.text $ Formatter.dateTime s.created ]
-         , onRowSelected: \snapshot -> do
+         , onRowSelected: \(Tuple idx snapshot) -> do
              hidePanelBodyFn
+             self.setState _ { selectedIdx = Just idx }
              self.props.onSnapshotSelected snapshot
+         , activeIdx: self.state.selectedIdx
          }
       , footer: empty
       }
