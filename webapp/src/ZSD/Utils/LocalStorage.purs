a281caadef300b7e1c000c092a5afb0133ccc030
j-keck <jhyphenkeck@gmail.com>
2020-02-14 14:05:51 +0100

    remove old debug-log entry

--- webapp/src/ZSD/Utils/LocalStorage.purs
+++ webapp/src/ZSD/Utils/LocalStorage.purs
@@ -8,7 +8,6 @@ import Prelude
 import Data.Either (hush)
 import Data.Maybe (Maybe)
 import Effect (Effect)
-import Effect.Console (log)
 import Effect.Uncurried (EffectFn1, EffectFn2, runEffectFn1, runEffectFn2)
 import Simple.JSON (class ReadForeign, class WriteForeign, readJSON, writeJSON)
 
@@ -16,7 +15,7 @@ foreign import setItem_ :: EffectFn2 String String Unit
 foreign import getItem_ :: EffectFn1 String String
 
 setItem :: forall a. WriteForeign a => String -> a -> Effect Unit
-setItem k v = log "setItem" *> (runEffectFn2 setItem_ k $ writeJSON v)
+setItem k v = (runEffectFn2 setItem_ k $ writeJSON v)
 
 getItem :: forall a. ReadForeign a => String -> Effect (Maybe a)
 getItem k = do
