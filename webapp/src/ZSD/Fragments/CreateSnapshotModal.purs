e1280d276f1097b6436075cdb3b0b1d81163d25e
j-keck <jhyphenkeck@gmail.com>
2020-04-13 09:52:16 +0200

    refactor create snapshot modal
      - use the 'Confirm' component

--- webapp/src/ZSD/Fragments/CreateSnapshotModal.purs
+++ webapp/src/ZSD/Fragments/CreateSnapshotModal.purs
@@ -1,16 +1,17 @@
 module ZSD.Fragments.CreateSnapshotModal where
 
 import Prelude
+
 import Data.Either (either)
 import Data.Foldable (foldMap)
-import Data.Maybe (Maybe(..), isNothing)
+import Data.Maybe (Maybe(..), isJust)
 import Data.Monoid (guard)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
-import React.Basic (Component, JSX, Self, createComponent, fragment, make)
+import React.Basic (Component, JSX, Self, createComponent, make)
 import React.Basic.DOM as R
-import React.Basic.DOM.Events (capture_)
+import ZSD.Components.Confirm as Confirm
 import ZSD.Fragments.SnapshotNameForm as SnapshotNameForm
 import ZSD.Model.Dataset (Dataset)
 import ZSD.Model.Dataset as Dataset
@@ -45,41 +46,15 @@ createSnapshotModal = make component { initialState, render }
   initialState = { snapshotName: Nothing }
 
   render self =
-    fragment
-      [ R.div
-          { className: "modal modal-show"
-          , children:
-            [ div "modal-dialog modal-dialog-centered"
-                $ div "modal-content"
-                $ fragment
-                    [ div "modal-header" $ R.text "Create ZFS Snapshot"
-                    , div "modal-body m-1"
-                        $ SnapshotNameForm.snapshotNameForm
-                            { dataset: self.props.dataset
-                            , defaultTemplate: self.props.snapshotNameTemplate
-                            , onNameChange: \n -> self.setState _ { snapshotName = n }
-                            , onEnter: \_ -> update self CreateSnapshot
-                            , onEsc: self.props.onRequestClose
-                            }
-                    , div "modal-footer"
-                        $ fragment
-                            [ R.button
-                                { className: "btn btn-secondary"
-                                , onClick: capture_ self.props.onRequestClose
-                                , children: [ R.text "Cancel" ]
-                                }
-                            , R.button
-                                { className:
-                                  "btn btn-primary"
-                                    <> guard (isNothing self.state.snapshotName) " disabled"
-                                , onClick: capture_ $ update self CreateSnapshot
-                                , children: [ R.text "Create" ]
-                                }
-                            ]
-                    ]
-            ]
-          }
-      , R.div { className: "modal-backdrop fade show" }
-      ]
-
-  div className child = R.div { className, children: [ child ] }
+    Confirm.confirm
+    { header: R.text "Create ZFS Snapshot"
+    , body: SnapshotNameForm.snapshotNameForm
+              { dataset: self.props.dataset
+              , defaultTemplate: self.props.snapshotNameTemplate
+              , onNameChange: \n -> self.setState _ { snapshotName = n }
+              , onEnter: \_ -> update self CreateSnapshot
+              , onEsc: self.props.onRequestClose
+              }
+    , onOk: guard (isJust self.state.snapshotName) $ update self CreateSnapshot
+    , onCancel: self.props.onRequestClose
+    }
